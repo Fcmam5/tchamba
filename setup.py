5e91c344c22c3076dc37cccddaf41f39bf97e5c3
fcmam5 <dr.fcmam5@gmail.com>
2017-01-02 18:40:04 +0100

    change to v 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'tchamba',
   packages = ['tchamba'],
-  version = '0.1',
+  version = '0.2',
   description = 'A random data generator',
   author = 'Fortas Abdeldjalil',
   author_email = 'dr.fcmam5@gmail.com',
